c25793dd03e7541c38656b32f2e3cc9eb723cadc
Kent WIlkison <wilkisonk.23@stu.materdei.org>
2020-01-16 17:20:17 -0800

    removed unused files

--- src/main/java/frc/robot/subsystems/Pickup.java
+++ src/main/java/frc/robot/subsystems/Pickup.java
@@ -1,15 +1,9 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.SensorCollection;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.consoles.Logger;
-import frc.robot.Constants.EncoderConstants;
-import frc.robot.Constants.TalonConstants;
-import frc.robot.EncoderUtils;
 import frc.robot.SubsystemDevices;
 
 // Pickup subsystem, for picking up balls
