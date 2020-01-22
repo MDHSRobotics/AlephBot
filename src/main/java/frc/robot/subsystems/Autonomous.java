e2cd07c6541df3ba1f2089ebe3a4ee27632d6cf5
Peter Le <lep.23@stu.materdei.org>
2020-01-21 16:40:03 -0800

    Added Move Forward Method

--- src/main/java/frc/robot/subsystems/Autonomous.java
+++ src/main/java/frc/robot/subsystems/Autonomous.java
@@ -4,11 +4,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.consoles.Logger;
 
+import static frc.robot.subsystems.Devices.talonSrxDiffWheelFrontLeft;
+import static frc.robot.subsystems.Devices.talonSrxDiffWheelFrontRight;
+import static frc.robot.subsystems.Devices.talonSrxDiffWheelRearLeft;
+import static frc.robot.subsystems.Devices.talonSrxDiffWheelRearRight;
+
 // Lighter subsystem, for turning lights on and off.
 public class Autonomous extends SubsystemBase {
 
     public Autonomous() {
         Logger.setup("Constructing Subsystem: Autonomous...");
+    }
+
+    public void moveForwardAuto() {
+        talonSrxDiffWheelFrontLeft.set(0.5);
+        talonSrxDiffWheelFrontRight.set(0.5);
+        talonSrxDiffWheelRearLeft.set(0.5);
+        talonSrxDiffWheelRearRight.set(0.5);
+
     }
 
     @Override
