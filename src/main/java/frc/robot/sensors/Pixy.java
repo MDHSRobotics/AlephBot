daa36819d7657de6236e58520380b70f5ad48a5d
Peter Le <lep.23@stu.materdei.org>
2020-02-25 18:40:49 -0800

    Fixed RGB NullPointerExpression

--- src/main/java/frc/robot/sensors/Pixy.java
+++ src/main/java/frc/robot/sensors/Pixy.java
@@ -44,6 +44,7 @@ public class Pixy {
             }
         }
         Pixy2Video video = BotSensors.pixy.getVideo();
+        rgb = video.new RGB(0, 0, 0);
         int detectedRGB = video.getRGB(5, 5, rgb, true);
         Logger.info("Pixy -> detectColor -> RGB: " + "R: " + rgb.getR() + "G: " + rgb.getG() + "B: " + rgb.getB());
         Logger.info("Pixy -> detectColor -> RGB: " + detectedRGB);
