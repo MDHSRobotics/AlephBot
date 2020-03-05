4cfe67908223d8e5053e140b6ad8fecf87b87b27
Peter Le <lep.23@stu.materdei.org>
2020-03-05 15:39:13 -0800

    Revert "Deleted Unnecessary Code"
    This reverts commit 85feb08a3a3067a0f663c14e089154b1b054fb61.

--- src/main/java/frc/robot/sensors/Pixy.java
+++ src/main/java/frc/robot/sensors/Pixy.java
@@ -1,9 +1,13 @@
 
 package frc.robot.sensors;
 
+import io.github.pseudoresonance.pixy2api.Pixy2CCC;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 import io.github.pseudoresonance.pixy2api.Pixy2Video;
 import io.github.pseudoresonance.pixy2api.Pixy2Video.RGB;
 import edu.wpi.first.wpilibj.DriverStation;
+
+import java.util.ArrayList;
 
 import frc.robot.consoles.Logger;
 import frc.robot.BotSensors;
@@ -12,16 +16,39 @@ public class Pixy {
 
     private static String detectedColor;
     private static RGB rgb;
+    private static final int blockSignature = 1;
 
     public static int colorCounter = 0;
     public static String colorMode;
 
     public static String detectColor() {
+        Pixy2CCC ccc = BotSensors.pixy.getCCC();
+        int blockCount = ccc.getBlocks(true, Pixy2CCC.CCC_SIG_ALL, 1);
+        if (blockCount <= 0) {
+            Logger.problem("Pixy -> detectColor -> No block count");
+        }
 
+        ArrayList<Block> blocks = ccc.getBlocks();
+        Block largestBlock = null;
+        if (blocks == null) {
+            Logger.info("Pixy -> detectColor -> No blocks");
+        }
+        for (Block block : blocks) {
+            if (block.getSignature() == blockSignature) {
+                if (largestBlock == null) {
+                    largestBlock = block;
+                }
+                else if (block.getWidth() > largestBlock.getWidth()) {
+                    largestBlock = block;
+                }
+            }
+        }
         Pixy2Video video = BotSensors.pixy.getVideo();
         rgb = video.new RGB(0, 0, 0);
+        int detectedRGB = video.getRGB(5, 5, rgb, true);
         BotSensors.pixy.setLamp((byte) 1, (byte) 0);
         Logger.info("Pixy -> detectColor -> RGB: " + "R: " + rgb.getR() + "G: " + rgb.getG() + "B: " + rgb.getB());
+        Logger.info("Pixy -> detectColor -> RGB: " + detectedRGB);
 
         // TODO: check values
         boolean redDetected = (((rgb.getR() > 0) && (rgb.getG() > 0) && (rgb.getB() == 0)) || ((rgb.getR() == 0) && (rgb.getG() > 0) && (rgb.getB() == 0)));
